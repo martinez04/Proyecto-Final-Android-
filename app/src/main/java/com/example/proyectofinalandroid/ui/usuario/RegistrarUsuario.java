1e957ffbe67b04bf924fd689b42b2ee1764ee41a
ruben-rc <ramoscubias09@gmail.com>
2020-11-27 21:07:03 -0600

    clase registrar usuario

--- app/src/main/java/com/example/proyectofinalandroid/ui/usuario/RegistrarUsuario.java
+++ app/src/main/java/com/example/proyectofinalandroid/ui/usuario/RegistrarUsuario.java
@@ -1,6 +1,59 @@
 package com.example.proyectofinalandroid.ui.usuario;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.example.proyectofinalandroid.R;
 
-public class RegistrarUsuario extends Activity {
+public class RegistrarUsuario extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public RegistrarUsuario() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RegistrarUsuario.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RegistrarUsuario newInstance(String param1, String param2) {
+        RegistrarUsuario fragment = new RegistrarUsuario();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registrar_usuario, container, false);
+    }
 }
